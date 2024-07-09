48199390ffe19154d8a4964f6928677b9dd43d77
chris <n1086402@ntub.edu.tw>
2024-07-09 18:41:15 +0800

    first commit

---
+++ src/main/java/com/tia102g1/member/model/Member.java
@@ -0,0 +1,19 @@
+package com.tia102g1.member.model;
+
+public class Member {
+	private Integer memberId;
+	private Integer memberLvId;
+	
+	public Integer getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(Integer memberId) {
+		this.memberId = memberId;
+	}
+	public Integer getMemberLvId() {
+		return memberLvId;
+	}
+	public void setMemberLvId(Integer memberLvId) {
+		this.memberLvId = memberLvId;
+	}
+}
