70cc0f664ed4166e8a66f61ffc92039aed8f2e7d
Evelyn Mei <yumei927@gmail.com>
2024-08-05 01:48:25 +0800

    :heavy_plus_sign: added checkOut.html, commonAsk.html, loginSignup.html, orderConfirmation.html, productCategory.html, sample.html, store.html. modified other frontend webpages, static.scss, styles.css

--- src/main/java/com/tia102g1/FrontendController.java
+++ src/main/java/com/tia102g1/FrontendController.java
@@ -9,10 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FrontendController {
 
     /* ======================= 前台使用者頁面 ======================= */
-    //首頁
+    // 首頁
     @RequestMapping({"/index","/index.html"})
     public String frontendIndex(Model model) {
         return "/frontendApp/index";
+    }
+
+    /*  ====== 會員  ====== */
+    // 註冊登入
+    @GetMapping({"/loginsignup", "/loginSignup.html"})
+    public String frontendLoginSignup(Model model) {
+        return "/frontendApp/loginSignup";
+    }
+    // 我的最愛
+    @GetMapping({"/favproduct","/favProduct.html"})
+    public String frontendFavProduct(Model model) { return "/frontendApp/favProduct"; }
+
+    /*  ====== 購物流程 ====== */
+    // 商品總覽
+    @GetMapping({"/productcategory", "/productCategory.html"})
+    public String frontendCategory(Model model) {
+        return "/frontendApp/productCategory";
+    }
+
+    // 商品詳細資訊
+    @GetMapping({"/productdetails","/productDetails.html"})
+    public String frontendProductDetail(Model model) {
+        return "/frontendApp/productDetails";
     }
 
     //購物車
@@ -21,25 +44,48 @@ public class FrontendController {
         return "/frontendApp/cart";
     }
 
-    //關於我們
-    @GetMapping({"/aboutus","/aboutUs.html"})
-    public String frontendAboutUs(Model model){
-        return "/frontendApp/aboutUs";
+    /* ====== 結帳付款 ====== */
+    // 填寫付款資訊
+    @GetMapping({"/checkout", "/checkout.html"})
+    public String frontendCheckout(Model model) {
+        return "/frontendApp/checkout";
     }
 
-    //聯絡我們
+    // 訂單成立確認
+    @GetMapping({"/orderconfirmation", "/orderConfirmation.html"})
+    public String frontendOrderConfirm(Model model) {
+        return "/frontendApp/orderConfirmation";
+    }
+
+    /* ====== 常見問題 ====== */
+    @GetMapping({"/commonask", "/commonAsk.html"})
+    public String frontendCommonAsk(Model model) {
+        return "/frontendApp/commonAsk";
+    }
+
+    /* ====== 客服 ====== */
+    // 聯絡我們
     @GetMapping({"/contactus","/contactUs.html"})
     public String frontendContactUs(Model model){
         return "/frontendApp/contactUs";
     }
 
-    //商品詳細資訊
-    @GetMapping({"/productdetails","/productDetails.html"})
-    public String frontendProductDetail(Model model) {
-        return "/frontendApp/productDetails";
+    /* ====== 門市資訊 ====== */
+    @GetMapping({"/store","/store.html"})
+    public String frontendStore(Model model) { return "/frontendApp/store"; }
+
+
+
+    //===================================================
+    //關於我們 (有空再做)
+    @GetMapping({"/aboutus","/aboutUs.html"})
+    public String frontendAboutUs(Model model){
+        return "/frontendApp/aboutUs";
     }
 
-    //我的最愛商品
-    @GetMapping({"/favproduct","/favProduct.html"})
-    public String frontendFavProduct(Model model) { return "/frontendApp/favProduct"; }
+    //測試頁
+    @GetMapping({"/sample"})
+    public String frontendSample(Model model) {
+        return "/frontendApp/sample";
+    }
 }
