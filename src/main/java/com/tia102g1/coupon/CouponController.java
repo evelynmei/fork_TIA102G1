a24f019f0b27bf64179f8625014281d00c204d37
Evelyn Mei <yumei927@gmail.com>
2024-08-11 00:19:02 +0800

    mainPageCoupon.html 完成新增, 刪除 application.properties 為了th:method="delete"的功能有bug而加

--- src/main/java/com/tia102g1/coupon/CouponController.java
+++ src/main/java/com/tia102g1/coupon/CouponController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,11 +22,6 @@ public class CouponController {
      * @return
      */
     @PostMapping("coupon/add")
-//    public ResponseEntity<Coupon> addCoupon(@RequestBody Coupon coupon) {
-//        Coupon newCoupon = couponService.addCoupon(coupon);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(newCoupon);
-//    }
-
     public String addCoupon(@ModelAttribute("coupon") Coupon coupon) {
         couponService.addCoupon(coupon);
         return "redirect:/coupon/mainPageCoupon";
@@ -37,20 +33,33 @@ public class CouponController {
      * @return
      */
     @PutMapping("coupon/{couponId}")
-    public ResponseEntity<Coupon> updateCoupon(@PathVariable Integer couponId, @RequestBody Coupon coupon) {
+    public String updateCoupon(@PathVariable Integer couponId, @ModelAttribute("coupon") Coupon coupon, RedirectAttributes redirectAttributes) {
         coupon.setCouponId(couponId);
-       Coupon updatedCoupon = couponService.updateCoupon(coupon);
-        return ResponseEntity.status(HttpStatus.OK).body(updatedCoupon);
-}
+        Coupon updatedCoupon = couponService.updateCoupon(coupon);
+
+        // 若需要傳遞更新後的優惠券信息到重定向的頁面，可以使用 RedirectAttributes
+        redirectAttributes.addFlashAttribute("updatedCoupon", updatedCoupon);
+
+        return "redirect:/coupon/mainPageCoupon";
+    }
+
+
+//    @PutMapping("coupon/{couponId}")
+//    public ResponseEntity<Coupon> updateCoupon(@PathVariable Integer couponId, @RequestBody Coupon coupon) {
+//        coupon.setCouponId(couponId);
+//       Coupon updatedCoupon = couponService.updateCoupon(coupon);
+//        return ResponseEntity.status(HttpStatus.OK).body(updatedCoupon);
+//}
     /**
      * 刪除優惠券
      * @param couponId
      * @return
      */
     @DeleteMapping("coupon/{couponId}")
-    public ResponseEntity<?> deleteCoupon(@PathVariable Integer couponId) {
+    public String deleteCoupon(@PathVariable Integer couponId) {
+        System.out.println("couponId = " + couponId);
         couponService.deleteCoupon(couponId);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return "redirect:/coupon/mainPageCoupon";
     }
 
     /**
